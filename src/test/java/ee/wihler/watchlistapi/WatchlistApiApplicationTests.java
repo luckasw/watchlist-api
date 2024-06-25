caa6c9eac62a2f0ed3e57c60822d992cc0e576a1
luckasw <Albert.Wihler@gmail.com>
2024-06-25 18:10:32 +0300

    Initial commit

---
+++ src/test/java/ee/wihler/watchlistapi/WatchlistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.wihler.watchlistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
