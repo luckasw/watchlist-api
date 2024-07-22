1ec14eb56a2aae6d23149ed24b5845f9bc401cef
luckasw <Albert.Wihler@gmail.com>
2024-07-22 14:26:03 +0300

    Added roles and user_roles tables. Seeded user and admin roles

--- src/main/resources/db/migration/V1__Initial.sql
+++ src/main/resources/db/migration/V1__Initial.sql
@@ -1,24 +1,40 @@
 CREATE TABLE IF NOT EXISTS users (
-                                              id SERIAL PRIMARY KEY,
-                                              username VARCHAR(255) NOT NULL,
-                                              password VARCHAR(255) NOT NULL,
-                                              email VARCHAR(255) NOT NULL,
-                                              created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+id SERIAL PRIMARY KEY,
+username VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS movies (
-                                               id SERIAL PRIMARY KEY,
-                                               title VARCHAR(255) NOT NULL,
-                                               description TEXT,
-                                               release_date DATE NOT NULL,
-                                               created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+id SERIAL PRIMARY KEY,
+title VARCHAR(255) NOT NULL,
+description TEXT,
+release_date DATE NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS watchlist (
-                                                  id SERIAL PRIMARY KEY,
-                                                  user_id INT NOT NULL,
-                                                  movie_id INT NOT NULL,
-                                                  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                                  FOREIGN KEY (user_id) REFERENCES users(id),
-                                                  FOREIGN KEY (movie_id) REFERENCES movies(id)
+id SERIAL PRIMARY KEY,
+user_id INT NOT NULL,
+movie_id INT NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (movie_id) REFERENCES movies(id)
 );
+
+CREATE TABLE IF NOT EXISTS roles (
+id SERIAL PRIMARY KEY,
+name VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS user_roles (
+user_id INT NOT NULL,
+role_id INT NOT NULL,
+PRIMARY KEY (user_id, role_id),
+FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
+);
+
+INSERT INTO roles (name) VALUES ('ROLE_USER');
+INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
