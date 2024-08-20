bca6e5c37151ff3c3e82ff85b9f23ea96ab8aca5
luckasw <Albert.Wihler@gmail.com>
2024-08-20 14:03:15 +0300

    feat(security): add cors configuration

--- src/main/java/ee/wihler/watchlistapi/security/WebSecurityConfig.java
+++ src/main/java/ee/wihler/watchlistapi/security/WebSecurityConfig.java
@@ -16,7 +16,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
+//TODO: Implement CSRF protection
 @Configuration
 @EnableMethodSecurity
 public class WebSecurityConfig {
@@ -53,17 +59,36 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .cors((cors) -> cors
+                        .configurationSource(corsConfigurationSource())
+                )
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers("/api/**")
+                )
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/api/auth/**").permitAll()
+                        auth.requestMatchers("/api/auth/*").permitAll()
                                 .anyRequest().authenticated()
+                )
+                .cors((cors) -> cors
+                        .configurationSource(corsConfigurationSource())
                 )
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
